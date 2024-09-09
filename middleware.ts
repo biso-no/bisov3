42c14710a48f6342da23de3071de11fcaecf629e
MHEien <markushei@hotmail.no>
2024-09-09 11:58:05 +0200

    feat(middleware): enhance access control for edit routes and add team verification for user permissions

--- middleware.ts
+++ middleware.ts
@@ -2,39 +2,51 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { cookies } from "next/headers";
 import { getAccount } from "./lib/admin/account";
+import { getTeams } from "./lib/server";
+import { Query } from "node-appwrite";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
   const session = req.cookies.get("x-biso-session");
-  let isUser: boolean;
-  let account: any; // Define account here to make it accessible
+  let account: any;
+  let canEditPages = false;
 
   if (session) {
     account = await getAccount();
     if (account.$id) {
-      isUser = true; // Use assignment
+      // Fetch teams that the user belongs to
+      const teams = await getTeams([Query.equal('name', ['admin', 'PR'])]);
+
+      // Check if the user is part of the "Admin" or "HR" teams
+      const userTeams = teams.teams.map(team => team.name);
+      canEditPages = userTeams.includes("admin") || userTeams.includes("PR");
     }
   }
 
-  if (req.method === "GET") {
-    // Rewrite routes that match "/[...puckPath]/edit" to "/puck/[...puckPath]"
-    if (req.nextUrl.pathname.endsWith("/edit")) {
-      const pathWithoutEdit = req.nextUrl.pathname.slice(
-        0,
-        req.nextUrl.pathname.length - 5
-      );
-      const pathWithEditPrefix = `/puck${pathWithoutEdit}`;
-
-      return NextResponse.rewrite(new URL(pathWithEditPrefix, req.url));
+  // If trying to access an "/edit" route, check if the user can edit
+  if (req.nextUrl.pathname.endsWith("/edit")) {
+    if (!canEditPages) {
+      return NextResponse.redirect(new URL("/", req.url)); // Redirect if not authorized
     }
 
-    // Disable "/puck/[...puckPath]"
-    if (req.nextUrl.pathname.startsWith("/puck")) {
-      return NextResponse.redirect(new URL("/", req.url));
-    } else if (req.nextUrl.pathname.startsWith("/admin") && !account) {
-      return NextResponse.redirect(new URL("/auth/login", req.url));
-    }
+    const pathWithoutEdit = req.nextUrl.pathname.slice(
+      0,
+      req.nextUrl.pathname.length - 5
+    );
+    const pathWithEditPrefix = `/puck${pathWithoutEdit}`;
+
+    return NextResponse.rewrite(new URL(pathWithEditPrefix, req.url));
+  }
+
+  // Disable "/puck/[...puckPath]" for unauthorized users
+  if (req.nextUrl.pathname.startsWith("/puck")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  // Redirect to login if trying to access "/admin" without being logged in
+  if (req.nextUrl.pathname.startsWith("/admin") && !account) {
+    return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
   return res;
