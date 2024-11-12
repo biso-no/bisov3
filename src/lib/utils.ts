5b468b589a4a702471dd3935a4b30dea5d4ccb6e
MHEien <markushei@hotmail.no>
2024-11-12 10:49:53 +0100

    Insert missing campusMap

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const campusMap = {
+  1: 'Oslo',
+  2: 'Bergen',
+  3: 'Trondheim',
+  4: 'Stavanger',
+  5: 'National'
+}
