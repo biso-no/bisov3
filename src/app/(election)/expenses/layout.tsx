5fb6e7f6c0ae1141992b75cfcc916330876baad1
Markus Heien <markushei@hotmail.no>
2025-02-07 23:34:52 +0100

    nav

--- src/app/(election)/expenses/layout.tsx
+++ src/app/(election)/expenses/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/globals.css'
 import { FormContextProvider, useFormContext  } from "./[expenseId]/formContext";
 import { AuthProvider } from '@/lib/hooks/useAuth';
+import UserLayout from '@/components/ui/nav';
 interface LayoutProps {
     children: React.ReactNode
 }
@@ -9,7 +10,11 @@ export default function Expenselayout({children}: LayoutProps) {
 
     return (
         <AuthProvider>
-        <FormContextProvider>{children}</FormContextProvider>
+        <FormContextProvider>
+            <UserLayout>
+            {children}
+            </UserLayout>
+            </FormContextProvider>
       </AuthProvider>
 
 
